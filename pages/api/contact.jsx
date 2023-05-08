f7353218caeb89640a37de97a421c54d4314067a
Abhaya <shankarabhaya9@gmail.com>
2023-05-08 17:33:24 +0530

    mongodb connection and api linked

--- pages/api/contact.jsx
+++ pages/api/contact.jsx
@@ -1,4 +1,6 @@
-function formHandler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function formHandler(req, res) {
   if (req.method === "POST") {
     const { email, name, message } = req.body;
 
@@ -26,7 +28,31 @@ function formHandler(req, res) {
       message,
     };
 
-    console.log(newMessage);
+    let client;
+
+    try {
+      client = await MongoClient.connect(
+        `mongodb+srv://Abhaya:${process.env.NEXT_PUBLIC_PASSWORD}@cluster0.rcblahe.mongodb.net/blog-app?retryWrites=true&w=majority`
+      );
+    } catch (error) {
+      res.status(500).json({ message: "Could not connect to DB" });
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection("messages").insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: "storing messages failed!" });
+      return;
+    }
+
+    client.close();
+
+    // console.log(newMessage);
 
     res.status(201).json({ message: "Success", message: newMessage });
   }
