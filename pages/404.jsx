c8eb4ab65c33b24d979221dc10914c489687de0b
Abhaya <shankarabhaya9@gmail.com>
2023-08-21 23:45:48 +0530

    bugs fixx

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
+import { redirect } from "next/navigation";
 
 const Error = () => {
   const [redirect, setRedirect] = useState("");
