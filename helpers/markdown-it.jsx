476f48e5ee3cb1d96459b493c1e56936cae211d8
Abhaya <shankarabhaya9@gmail.com>
2023-09-11 23:36:21 +0530

    refractoring

--- helpers/markdown-it.jsx
+++ helpers/markdown-it.jsx
@@ -5,13 +5,6 @@ import markdownAnchor from "markdown-it-anchor";
 import markdownIframe from "markdown-it-iframe";
 import slugify from "slugify";
 
-// const hljs = require("highlight.js/lib/core");
-
-// hljs.registerLanguage(
-//   "javascript",
-//   require("highlight.js/lib/languages/javascript")
-// );
-
 export const md = new MarkdownIt({
   breaks: true,
 })
@@ -32,8 +25,3 @@ export const md = new MarkdownIt({
   .use(markdownIframe, {
     allowfullscreen: true,
   });
-
-// export const sanitize = (html) => {
-//   const result = html.replace(/<a href+/g, "<a target='_blank' href");
-//   return result;
-// };
