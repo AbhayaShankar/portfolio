b19045bb477baa9968a8737982e93d812749f6d8
Abhaya <shankarabhaya9@gmail.com>
2023-10-16 22:57:30 +0530

    500 error page

---
+++ pages/500.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import error from "../public/404.png";
+import Image from "next/image";
+
+const Error = () => {
+  const router = useRouter();
+  const [redirect, setRedirect] = useState("6");
+
+  useEffect(() => {
+    let time = 5;
+    const timer = setInterval(() => {
+      if (time > 0) {
+        setRedirect(time);
+        time--;
+      } else {
+        router.push("/");
+      }
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  return (
+    <div className="error">
+      <h1>Oops! The page you are trying to find is unavailable.</h1>
+      <p>Redirecting back to home in {redirect} </p>
+      <Image src={error} width={250} height={250} alt="error-img" />
+    </div>
+  );
+};
+
+export default Error;
