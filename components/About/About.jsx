8b1dc59beb39b321d612d7f6b9e06164284bdedc
Abhaya Shankar <shankarabhaya9@gmail.com>
2024-03-05 11:39:53 +0530

    about update

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -13,14 +13,15 @@ const About = () => {
       <div className={classes.intro}>
         <h3>Hey! Folks on the Web...</h3>
         <p>
-          I'm Abhaya, an India based professional Frontend Developer with around
-          2 years of experience in ReactJS/NextJS Development. I specialize in
-          creating responsive user interfaces for web-based applications that
-          prioritize design and user experience.
+          I'm Abhaya, an India based professional Frontend Developer with{" "}
+          <strong>2+ </strong>
+          years of experience in ReactJS/NextJS Development with typescript. I
+          specialize in creating responsive user interfaces for web-based
+          applications that prioritize design and user experience.
           <br />
           <br />I love building projects and contributing to Open Source. I am
           learning and getting more hands-on expertise on Backend Technologies
-          using Express JS.
+          using Express JS, WebSockets and WebRTC.
           <br />
           <br />
           Currently I am working at Capgemini Inc. as a React Frontend Developer
@@ -39,6 +40,10 @@ const About = () => {
           <br />
           After this I also worked as a frontend developer at Memsaab - an
           online Saree Clothing store.
+          <br />
+          <br />
+          In my most recent project, I have developed a full stack app for a gym
+          - fitness hub using NextJS with Typescript.
           <br />
           <br />I am currently immersed in learning Express and Node.js, which
           is paving the way for my journey as a Full Stack Developer.
